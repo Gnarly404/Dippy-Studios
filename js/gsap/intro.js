0f0de8c4ed86729ca236b1000ea47532a1e2cd90
Dippy Studios <dev@dippystudios.com>
2026-07-18 05:10:20 +0000

    Guard intro animation targets to stop harmless GSAP warnings on non-homepage pages

--- js/gsap/intro.js
+++ js/gsap/intro.js
@@ -6,13 +6,19 @@ export function playIntro() {
 
   const tl = gsap.timeline({ defaults: { ease: 'power3.out' } });
 
-  tl.from('.navbar', { y: -40, opacity: 0, duration: 0.7 })
-    .from('[data-split-text] .line span', {
+  if (document.querySelector('.navbar')) {
+    tl.from('.navbar', { y: -40, opacity: 0, duration: 0.7 });
+  }
+  if (document.querySelector('[data-split-text] .line span')) {
+    tl.from('[data-split-text] .line span', {
       yPercent: 110,
       duration: 0.9,
       stagger: 0.06,
-    }, '-=0.3')
-    .from('.hero__meta > *', { y: 20, opacity: 0, duration: 0.6, stagger: 0.08 }, '-=0.4');
+    }, '-=0.3');
+  }
+  if (document.querySelector('.hero__meta > *')) {
+    tl.from('.hero__meta > *', { y: 20, opacity: 0, duration: 0.6, stagger: 0.08 }, '-=0.4');
+  }
 
   return tl;
 }
